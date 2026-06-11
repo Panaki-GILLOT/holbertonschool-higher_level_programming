db66915e282c9dfa14df027d7e8be9590399e8f6
panaki-gillot <gillotpanaki@gmail.com>
2026-06-11 18:01:25 +0200

    feat: add task_02_requests.py to fetch and print posts from an API and save them to a CSV file

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,10 +1,18 @@
+#!/usr/bin/python3
+"""
+Defines a City class and Base using SQLAlchemy ORM
+"""
+
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
 
 class City(Base):
+    """
+    City class mapped to cities table
+    """
     __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
