be3a334737c2b520a65070393a9c404fdf2802bc
panaki-gillot <gillotpanaki@gmail.com>
2026-07-03 11:20:57 +0200

    feat: add 6-multi_languages_loop.js script to display programming languages

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let output = '';
+
+for (const language of languages) {
+  output += language + '\n';
+}
+
+console.log(output.trimEnd());
